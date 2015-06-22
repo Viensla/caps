06a28836264ab05fc70c2c270e47f61fba189b7f
mac viens-la 2 <macviens-la2@imac-de-mac.home>
2015-06-22 14:12:09 +0200

    en version

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,6 +60,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.registerTask('default', ['concat','uglify']);
+    grunt.registerTask('default', ['concat', 'uglify']);
+//    grunt.registerTask('default', ['compass']);
 
 };
